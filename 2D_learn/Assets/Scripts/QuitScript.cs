e1df20a3ac7bacfe5a0e9eb13ac8ffaa47ceb587
play <t.pauljabez@gamil.com>
2023-06-22 15:50:37 +0530

    Quit button enabled

--- 2D_learn/Assets/Scripts/QuitScript.cs
+++ 2D_learn/Assets/Scripts/QuitScript.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndMenu : MonoBehaviour
+public class QuitScript : MonoBehaviour
 {
     public void Quit()
     {
