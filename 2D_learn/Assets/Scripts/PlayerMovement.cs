295499d6684ea6bc8fd39af858e25ec16cd0b994
play <t.pauljabez@gamil.com>
2023-06-22 14:06:13 +0530

    Update PlayerMovement.cs

--- 2D_learn/Assets/Scripts/PlayerMovement.cs
+++ 2D_learn/Assets/Scripts/PlayerMovement.cs
@@ -8,23 +8,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
     private Animator anim;
     private BoxCollider2D coll;
+
+    private float dirX = 0f;
 
     [SerializeField] private LayerMask jumpableGround;
 
     [SerializeField] private AudioSource jumpSoundEffect;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         coll = GetComponent<BoxCollider2D>();
     }
 
-
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump") && IsGrounded())  //To avoid flying when space is held on for long
@@ -33,19 +37,27 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 10f);    //setting speed for player
         }
 
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
+    {
         if (dirX > 0f) //to check if running
         {
             anim.SetBool("running", true);
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
             anim.SetBool("running", true);
+            sprite.flipX = true;
         }
         else
         {
             anim.SetBool("running", false);
         }
     }
+
     private bool IsGrounded()   //to be able to jump only when above the ground
     {
         //creating a new box and checking if it overlaps with the ground
